b2649cbd09b458b26bc3d22451089c2a2c439d72
Ümit Ekrem Mikyas <mikyasstudio@gmail.com>
2026-06-17 12:31:15 +0300

    Sistem genelindeki tüm versiyon numaraları 0.97.4.55 olarak senkronize edildi

---
+++ hafizana_guven/sync_versions.ps1
@@ -0,0 +1,46 @@
+$ErrorActionPreference = "Stop"
+$dir = ".\";
+$newVersion = "0.97.4.55"
+
+Write-Host "Updating version.json..."
+$vJsonPath = Join-Path $dir "version.json"
+$vJson = Get-Content $vJsonPath -Raw -Encoding UTF8 | ConvertFrom-Json
+$vJson.version = $newVersion
+$vJson.changelog = "Tüm sistemlerde versiyon numaraları eşitlendi ve senkronize edildi."
+$vJson | ConvertTo-Json -Depth 5 | Set-Content $vJsonPath -Encoding UTF8
+
+Write-Host "Updating package.json..."
+$pJsonPath = Join-Path $dir "package.json"
+$pJson = Get-Content $pJsonPath -Raw -Encoding UTF8 | ConvertFrom-Json
+$pJson.version = $newVersion
+$pJson | ConvertTo-Json -Depth 5 | Set-Content $pJsonPath -Encoding UTF8
+
+Write-Host "Updating index.html..."
+$htmlPath = Join-Path $dir "index.html"
+(Get-Content $htmlPath -Encoding UTF8) -replace '\?v=\d+\.\d+\.\d+\.\d+', "?v=$newVersion" | Set-Content $htmlPath -Encoding UTF8
+
+Write-Host "Updating .last_sent_version.txt..."
+$lvPath = Join-Path $dir ".last_sent_version.txt"
+Set-Content $lvPath -Value "v$newVersion" -Encoding UTF8
+
+Write-Host "Updating changelog.txt..."
+$changelogPath = Join-Path $dir "changelog.txt"
+$changelogContent = Get-Content $changelogPath -Encoding UTF8
+$newEntry = @"
+[v$newVersion] - 17.06.2026
+
+Genel:
+- Sistem genelindeki tüm versiyon numaraları (index.html, package.json, version.json) senkronize edildi ve eşitlendi.
+
+"@
+
+$newChangelog = @()
+$newChangelog += $changelogContent[0] # Header
+$newChangelog += ""
+$newChangelog += $newEntry
+for ($i=1; $i -lt $changelogContent.Length; $i++) {
+    $newChangelog += $changelogContent[$i]
+}
+$newChangelog | Set-Content $changelogPath -Encoding UTF8
+
+Write-Host "Tüm sürümler v$newVersion olarak eşitlendi!"
