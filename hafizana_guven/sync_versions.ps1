b752826247960ad0ca31579e07f9d53c4baf4d18
Ümit Ekrem Mikyas <mikyasstudio@gmail.com>
2026-07-29 10:57:49 +0300

    Sürüm güncelleme tarihi bugünün tarihi (29 Temmuz 2026) olarak değiştirildi ve sync_versions.ps1 dosyası dinamik tarih alacak şekilde düzeltildi

--- hafizana_guven/sync_versions.ps1
+++ hafizana_guven/sync_versions.ps1
@@ -26,8 +26,9 @@ Set-Content $lvPath -Value "v$newVersion" -Encoding UTF8
 Write-Host "Updating changelog.txt..."
 $changelogPath = Join-Path $dir "changelog.txt"
 $changelogContent = Get-Content $changelogPath -Encoding UTF8
+$todayDate = (Get-Date).ToString('dd.MM.yyyy')
 $newEntry = @"
-[v$newVersion] - 17.06.2026
+[v$newVersion] - $todayDate
 
 Genel:
 - Sistem genelindeki tüm versiyon numaraları (index.html, package.json, version.json) senkronize edildi ve eşitlendi.
